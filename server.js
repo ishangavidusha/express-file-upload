142ad6eb3be33400b61ff876214bbd1542d827be
Ishanga Vidusha <ishangavidusha@gmail.com>
2021-01-14 11:31:26 +0900

    Change Port

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 
 global.__basedir = __dirname;
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
 app.use(cors());
 
 const initRoutes = require("./src/routes");
@@ -15,7 +11,7 @@ const initRoutes = require("./src/routes");
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = 8080;
+let port = 3000;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
